51803fa6948e8e986048d3cd945f59735a81c178
Adam MARONI <amaroni@e3r8p15.clusters.42paris.fr>
2021-12-15 16:21:02 +0100

    MAJ des tests et des srcs

--- main.c
+++ main.c
@@ -6,11 +6,44 @@
 /*   By: amaroni <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 14:51:22 by amaroni           #+#    #+#             */
-/*   Updated: 2021/12/14 09:19:47 by amaroni          ###   ########.fr       */
+/*   Updated: 2021/12/15 15:33:31 by amaroni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+int ft_is_input_okay(int argc, char **argv, char **envp)
+{
+	int i;
+	int rt;
+	char *cmd;
+	char *cmd_full_path;
+
+	i = 2;
+	cmd = NULL;
+	cmd_full_path = NULL;
+	rt = 1;
+	if (argc != 5)
+		return (0);
+	if (access(argv[1], F_OK) == -1)
+		return (0);
+	if (access(argv[argc - 1], F_OK) == -1)
+		return (0);
+	while (i < argc - 1)
+	{
+		cmd = ft_extract_cmd(argv[i]);
+		cmd_full_path = ft_return_cmd_absolute_path(cmd, ft_extract_envar_path(envp));
+		if (access(cmd_full_path, F_OK) == -1)
+			rt = 0;
+		free(cmd);
+		free(cmd_full_path);
+		i++;
+	}
+	return (rt);
+}
+
+
+
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -18,10 +51,10 @@ int	main(int argc, char **argv, char **envp)
 	int pid;
 	int pid2;
 	int fd;
-	int fd2;
 	t_execve_data *data;
 
-	if (argc != 5)
+	//is input alright
+	if (!ft_is_input_okay(argc, argv, envp))
 	{
 		printf("Error\n");
 		return (1);
@@ -34,6 +67,8 @@ int	main(int argc, char **argv, char **envp)
 	if (pid == 0)
 	{
 		fd = open("inFile", O_RDWR, 0777);
+		if (fd == -1)
+			return (5);
 		dup2(fd, STDIN_FILENO);
 		dup2(pipefd[1], STDOUT_FILENO);
 		data = ft_return_execve(argv[2], envp);
@@ -46,15 +81,17 @@ int	main(int argc, char **argv, char **envp)
 	close(pipefd[1]);
 	pid2 = fork();
 	if (pid2 == -1)
-		return (4);
+		return (5);
 	if (pid2 == 0)
 	{
-		fd2 = open("outFile", O_RDWR, 0777);
-		dup2(fd2, STDOUT_FILENO);
+		fd = open("outFile", O_RDWR, 0777);
+		if (fd == -1)
+			return (6);
+		dup2(fd, STDOUT_FILENO);
 		dup2(pipefd[0], STDIN_FILENO);
 		data = ft_return_execve(argv[3], envp);
 		execve(data->cmd, data->tab, envp);
-		close(fd2);
+		close(fd);
 		close(pipefd[1]);
 		close(pipefd[0]);
 		ft_free_execve_data(data);
--- tests_validation.sh
+++ tests_validation.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-export GREP_COLORS='ms=01;32'
+OUT='./pipex'
+
+#MEM_CHECK
+LOGFILE="logfile"
+
+#GREP SETTINGS
+RED='ms=01;31'
+GREEN='ms=01;32'
+
+function error_testing () {
+	echo -n "$OUT $1 >> "
+	export GREP_COLORS=$GREEN
+	valgrind --log-file=$LOGFILE $OUT $1 | grep --color=always -i "Error"
+	export GREP_COLORS=$RED
+	grep -A 5 --color=always "LEAK SUMMARY" $LOGFILE
+	rm -f $LOGFILE
+}
+
+#test_inFile cmd1 cmd2 test_outFile
+function functionnal_testing()
+{
+	TMP_INFILE="tmp_$1"
+	TMP_OUTFILE="tmp_$4"
+	touch $TMP_OUTFILE
+	cp $1 $TMP_INFILE
+	< $TMP_INFILE $2 | $3 > $TMP_OUTFILE
+	valgrind --log-file=$LOGFILE $OUT $1 $2 $3 $4
+	export GREP_COLORS=$RED
+	grep -A 5 --color=always "LEAK SUMMARY" $LOGFILE
+	export GREP_COLORS=$GREEN
+	echo -n "$OUT $1 $2 $3 $4 >> "
+	diff -s $4 $TMP_OUTFILE | grep --color=always "identical"
+	rm -f $TMP_INFILE $TMP_OUTFILE $LOGFILE
+}
 
 make -s
-echo "Should return error"
-printf "./pipex 1 2 3 4 5 >> "
-./pipex 1 2 3 4 5 | grep --color=always "Error"
-printf "./pipex 1 2 3 4 >> "
-./pipex 1 2 3 4 | grep --color=always "Error"
-printf "./pipex inFile 2 3 4 >> "
-./pipex inFile 2 3 4 | grep --color=always "Error"
-printf "./pipex 1 2 3 outFile >> "
-./pipex 1 2 3 outFile| grep --color=always "Error"
-echo "Should not return error"
-./pipex inFile 2 3 outFile
-
-
-
-
-
-
-
-export GREP_COLORS='ms=01;31'
-
-
-
-
-
-
+echo "----------Should return error----------"
+#CASE 1 2 3 4 5
+	error_testing "1 2 3 4 5"
+#CASE 1 2 3 4
+	error_testing "1 2 3 4"
+#CASE inFile 2 3 4
+	error_testing "inFile 2 3 4"
+#CASE 1 2 3 outFile
+	error_testing "1 2 3 outFile"
+#CASE inFile 2 3 outFile
+	error_testing "inFile 2 3 outFile"
+#CASE inFile cat 3 outFile
+	error_testing "inFile cat 3 outFile"
+#CASE inFile 2 wc outFile
+	error_testing "inFile 2 wc outFile"
+echo "----------Functionnal testing----------"
+#CASE inFile cat wc outFile
+	functionnal_testing "inFile" "cat" "wc" "outFile"
 
